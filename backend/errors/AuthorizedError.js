47a49332d7aeaf33dbafd3d8a648cb946315a65e
Alexandr Ukhanov <ukhanov1996@yandex.ru>
2022-12-09 21:33:40 +0700

    rm all

--- backend/errors/AuthorizedError.js
+++
@@ -1,9 +0,0 @@
-class AuthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'AuthorizedError';
-    this.statusCode = 401;
-  }
-}
-
-module.exports = AuthorizedError;
