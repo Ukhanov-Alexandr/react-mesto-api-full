3b2902619e8fee6e605e23c6c7be47579ac56be9
Alexandr Ukhanov <ukhanov1996@yandex.ru>
2022-12-09 22:57:35 +0700

    add front

---
+++ frontend/src/components/EditAvatarPopup.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PopupWithForm from "./PopupWithForm";
+
+function EditAvatarPopup({isOpen, onClose, onUpdateAvatar, isRequesting, handleOverlayClick}){
+  const avatarRef = React.useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onUpdateAvatar(avatarRef.current.value)
+  }
+
+  return (
+    <PopupWithForm
+      title="Обновить аватар"
+      name="avatar"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      handleOverlayClick={handleOverlayClick}
+    >
+      <input
+        className="popup__input popup__input_type_link"
+        name="input-link"
+        type="url"
+        placeholder="Ссылка на картинку"
+        required
+        ref={avatarRef}
+      />
+      <span className="form__input-error input-link-error"></span>
+      <button className="popup__btn-save form__submit" type="submit" >
+        {isRequesting ? 'Сохранение..' : "Сохранить"}
+      </button>
+    </PopupWithForm>
+  );
+}
+
+export default React.memo(EditAvatarPopup);
---
+++ frontend/src/components/InfoTooltip.js
@@ -0,0 +1,27 @@
+import React from "react";
+import regsuc from "../images/reg1.png";
+import regfail from "../images/reg-faild.svg";
+
+function InfoTooltip({ isOpen, onClose, loggedIn, handleOverlayClick }) {
+
+  return (
+    <div
+      className={`popup infoTooltip ${isOpen ? "popup_opened" : ""}`}
+      onMouseDown={handleOverlayClick}
+    >
+      <div className="popup__container infoTooltip__container">
+        <button
+          className="popup__btn-close"
+          type="button"
+          onClick={() => onClose()}
+        ></button>
+        <img className="infoTooltip__image " src={loggedIn ? regsuc : regfail} alt="логотип" />
+        <h3 className="infoTooltip__title">
+            {loggedIn ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.' }
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default InfoTooltip;
