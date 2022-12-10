7652c4a32a4994b9098fb54f0605995753eb6a36
Alexandr Ukhanov <ukhanov1996@yandex.ru>
2022-12-10 17:19:55 +0700

    fix heart

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,7 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 const Card = ({ card, onClick, onCardLike, onTrashClick }) => {
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner._id === currentUser._id;
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((id) => id === currentUser._id);
   function handleImageClick() {
     onClick(card);
   }
